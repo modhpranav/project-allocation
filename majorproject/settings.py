9c58aa63d494fab2aee0375e788ae285afa0066f
Pranav Modh <modhpranav21@gmail.com>
2024-04-30 17:21:05 -0400

    [ADD] Logfire

--- majorproject/settings.py
+++ majorproject/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os, dj_database_url
-
+import logfire
+from opentelemetry.instrumentation.django import DjangoInstrumentor
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -172,3 +173,6 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+logfire.configure()
+DjangoInstrumentor().instrument()
